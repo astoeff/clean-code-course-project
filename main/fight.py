10ee25b3cf9828620524e4900a953e2b5983006d
A.Stoev <antoni.1998@abv.bg>
2020-07-15 16:21:05 +0300

    Add attacking functionallity for models and main process in fight

--- main/fight.py
+++ main/fight.py
@@ -1,4 +1,7 @@
-from constants import DIRECTIONS_WITH_THEIR_OPPOSITES_DICTIONARY, FIGHT_INITIAL_INFORMATION_PART
+from constants import (DIRECTIONS_WITH_THEIR_OPPOSITES_DICTIONARY, FIGHT_INITIAL_INFORMATION_PART,
+                       PLAYER_ZERO_DAMAGE_WHEN_ATTACKING, FIGHT_HERO_ATTACK_INFORMATION_PART,
+                       FIGHT_HERO_CANNOT_ATTACK_INFORMATION_PART, FIGHT_ENEMY_ATTACK_INFORMATION_PART,
+                       FIGHT_ENEMY_CANNOT_ATTACK_INFORMATION_PART)
 
 
 class Fight:
@@ -14,10 +17,30 @@ class Fight:
         return DIRECTIONS_WITH_THEIR_OPPOSITES_DICTIONARY[self.direction]
 
     def hero_attack(self):
-        pass
+        damage_from_attack = self.hero.attack()
+        can_hero_attack = damage_from_attack > PLAYER_ZERO_DAMAGE_WHEN_ATTACKING
+        if can_hero_attack:
+            self.information_parts.append(FIGHT_HERO_ATTACK_INFORMATION_PART + damage_from_attack)
+        else:
+            self.information_parts.append(FIGHT_HERO_CANNOT_ATTACK_INFORMATION_PART)
+        self.enemy.take_damage(damage_from_attack)
 
     def enemy_attack(self):
-        pass
+        damage_from_attack = self.enemy.attack()
+        can_enemy_attack = damage_from_attack > PLAYER_ZERO_DAMAGE_WHEN_ATTACKING
+        if can_enemy_attack:
+            self.information_parts.append(FIGHT_ENEMY_ATTACK_INFORMATION_PART + damage_from_attack)
+        else:
+            self.information_parts.append(FIGHT_ENEMY_CANNOT_ATTACK_INFORMATION_PART)
+        self.hero.take_damage(damage_from_attack)
 
     def execute(self):
-        pass
+        is_fight_in_progress = self.hero.is_alive() and self.enemy.is_alive()
+        while is_fight_in_progress:
+            if self.hero.is_alive():
+                self.hero_attack()
+            if self.enemy.is_alive():
+                self.enemy_attack()
+
+    def get_fight_information(self):
+        return self.information_parts
--- main/models/playable.py
+++ main/models/playable.py
@@ -55,7 +55,20 @@ class Playable:
             raise ValueError(INVALID_SPELL_VALUE_ERROR_TEXT)
         self.spell = spell
 
+    def can_attack_with_weapon(self):
+        return self.weapon is not None
+
     def can_cast_spell(self):
         does_playable_have_spell = self.spell is not None
         does_playable_have_mana_for_casting_spell = self.spell.mana_cost <= self.mana_cost
         return does_playable_have_spell and does_playable_have_mana_for_casting_spell
+
+    def attack(self):
+        weapon_damaage = 0
+        spell_damage = 0
+        if self.can_attack_with_weapon():
+            weapon_damaage = self.weapon.damage_points
+        if self.can_cast_spell():
+            spell_damage = self.spell.damage_points
+        max_damage = max(weapon_damaage, spell_damage)
+        return max_damage
